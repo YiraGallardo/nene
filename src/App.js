82f6da1bedbf75b715cdec1edb3e4fbc03e9eabf
Yira Mac <yiramac@Yiras-MacBook-Pro.local>
2022-12-05 21:48:03 -0600

    mi primer avance

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div className="seccion-1">
+        <div className="header-content">
+          <div className="hamburguesa">
+            <div className="linea"></div>
+            <div className="linea"></div>
+            <div className="linea"></div>
+          </div>
+          <div className="logo-img">
+            <img src="img/logo.png" alt="Logo NENE" />
+            <div className="slogan">
+              <h1>ANTOJITOS NACIONALES</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="seccion-2">Negro</div>
+    </div>
+  );
+}
+
+export default App;
